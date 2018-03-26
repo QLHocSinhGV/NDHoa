4d126b0c0a4469de58251bd505589d2bafc683dd
duchoa97 <duchoa97@gmail.com>
2018-03-26 15:13:44 +0700

    Hoàn thiện CSDL
    Hoàn thiện cơ sở dữ liệu, tạo form Học lực

---
+++ frmHocLuc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using QLHocSinhTHPT.Controller;
+using QLHocSinhTHPT.Component;
+using DevComponents.DotNetBar;
+
+namespace QLHocSinhTHPT
+{
+    public partial class frmHocLuc : Office2007Form
+    {
+        #region Fields
+        HocLucCtrl  m_HocLucCtrl    = new HocLucCtrl();
+        QuyDinh     quyDinh         = new QuyDinh();
+        #endregion
+        #region Costructor
+        public frmHocLuc()
+        {
+            InitializeComponent();
+            DataService.OpenConnection();
+        }
+        #endregion
+
+        #region Load
+        private void frmHocLuc_Load(object sender, EventArgs e)
+        {
+            m_HocLucCtrl.HienThi(dGVHocLuc, bindingNavigatorHocLuc);
+        }
+        #endregion
+
+        #region BindingNavigatorItems
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            if (dGVHocLuc.RowCount == 0)
+                bindingNavigatorDeleteItem.Enabled = false;
+
+            else if (MessageBoxEx.Show("Bạn có chắc chắn xóa dòng này không?", "DELETE", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                bindingNavigatorHocLuc.BindingSource.RemoveCurrent();
+            }
+        }
+
+        private void bindingNavigatorExitItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }      
+}
